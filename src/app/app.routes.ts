d2c4c9968a317cd6ba037d8d1a0a2693a3ddd4e4
Nathandiv <k.n.hlungwani@gmail.com>
2024-09-18 11:28:00 +0200

    added routing and components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AuthenticatorComponent } from './Pages/authenticator/authenticator.component';
+import { SidebarComponent } from './UI/Shared-UI/sidebar/sidebar.component';
+import { HomeComponent } from './Pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',redirectTo:'authenticator',pathMatch:'full'},
+  {path:'authenticator',component:AuthenticatorComponent},
+  {
+    path:'',component:SidebarComponent,
+
+    children:[{
+      path:'home',component:HomeComponent
+    }]
+  }
+];
